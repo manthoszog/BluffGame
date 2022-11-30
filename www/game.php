3ea237b6d79d37defe8df299aec7bac6ae09f77b
Manthos Zografos <116436116+manthoszog@users.noreply.github.com>
2022-11-30 15:00:54 +0200

    edit game.php

--- www/game.php
+++ www/game.php
@@ -6,8 +6,16 @@
 	$input = json_decode(file_get_contents('php://input'),true);
 
     switch($req=array_shift($request)){
-        case '':
-
+        case 'cards':
+            switch($req2=array_shift($request)){
+                case '':
+                case null:
+                    if($method == 'GET'){
+                        $mysqli;
+                        $query = 'select * from player1_karta';
+                    }
+                    break;
+            }
             break;
         default: 
             header("HTTP/1.1 404 Not Found");
